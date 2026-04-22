3752f44bf596621de37434d39fc2ef6048e8f452
Fassam Khan <muhammadfassam465@gmail.com>
2026-04-22 23:04:18 +0500

    percentage added

--- Jschp(38-48)/app.js
+++ Jschp(38-48)/app.js
@@ -1,75 +1,108 @@
 // Power Fuction 
 
 
-const powerFunc = ()=>{
+const powerFunc = () => {
     let result = 1
 
     let base = +prompt("Enter the base number : ")
     const power = +prompt("Enter power number ")
 
-    for( i = 1; i <= power; i++){
+    for (i = 1; i <= power; i++) {
         result = result * base
 
 
 
     }
 
-    alert(result) 
+    alert(result)
 
 
-result = 1
+    result = 1
 
 }
 
 
-const leapYear = ()=>{
+const leapYear = () => {
 
     let year = +prompt("Enter your year : ");
-    if(year % 4 === 0){
+    if (year % 4 === 0) {
         alert(year + " is a leap year ")
     }
-    else{
+    else {
         alert("Not a leap")
     }
 
 }
 
-const withDraw = ()=>{
+const withDraw = () => {
     note10 = 0
     note50 = 0
     note100 = 0
     let amount = +prompt("Enter amount:");
-    if(amount >= 100){
+    if (amount >= 100) {
         var note100 = Math.floor(amount / 100)
         console.log(note100)
         amount = amount % 100
 
     }
-    if(amount >= 50 && amount < 100){
+    if (amount >= 50 && amount < 100) {
         var note50 = Math.floor(amount / 50)
-        console.log( note50)
+        console.log(note50)
         amount = amount % 50
 
     }
 
-    if(amount < 50 && amount > 0 ){
+    if (amount < 50 && amount > 0) {
         var note10 = Math.floor(amount / 10)
         console.log(note10)
     }
 
 
     alert("you will have " + `${note100} hundred notes ${note50} fifty notes ${note10} ten notes`)
- 
+
 }
 
-const areaOfTriangle = ()=>{
+const areaOfTriangle = () => {
     let a = +prompt("Enter triangle first length ");
     let b = +prompt("Enter triangle Second Length ");
-    let c  = +prompt("Enter triangle Third Length ");
+    let c = +prompt("Enter triangle Third Length ");
 
-    s = (a + b + c) / 2;
+    let s = (a + b + c) / 2;
 
     area = s * (s - a) * (s - b) * (s - c)
 
     alert("area is " + area)
 }
+
+const studentMarkshhet = () => {
+    let subEnglish = +prompt("Enter English Sub Marks ");
+    let subUrdu = +prompt("Enter Urdu Subject Marks ");
+    let subMath = +prompt("Enter Math Subject Marks ");
+
+
+    const average = (subEnglish, subUrdu, subMath) => {
+        let totalMarks = subEnglish + subUrdu + subMath;
+
+        return totalMarks / 3;
+
+    }
+
+    const percentage = (subEnglish, subUrdu, subMath) => {
+        let totalMark = 300;
+        let obtainMark = subEnglish + subUrdu + subMath
+
+        
+        return  (obtainMark / totalMark) * 100;
+    }
+
+
+
+    let averageVal = average(subEnglish, subUrdu, subMath)
+    let percentVal = percentage(subEnglish, subUrdu, subMath)
+
+
+
+    alert("The average is " + averageVal.toFixed(0, 3) + "percentage is " + percentVal.toFixed(0, 2))
+
+}
+
